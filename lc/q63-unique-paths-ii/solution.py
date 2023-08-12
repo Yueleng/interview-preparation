3496fa5dbe827b8dd669dbe8fd39d282c370a58a
Alan <wangyueleng@gmail.com>
2023-08-12 11:16:56 -0400

    lc/q63-unique-paths-ii

---
+++ lc/q63-unique-paths-ii/solution.py
@@ -0,0 +1,14 @@
+from ast import List
+
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        n = len(obstacleGrid)
+        m = len(obstacleGrid[0])
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        dp[1][1] = 1 - obstacleGrid[0][0]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if not obstacleGrid[i - 1][j - 1]:
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j] + dp[i][j - 1])
+        return dp[-1][-1]
