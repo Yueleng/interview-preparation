bd98ba50e82960caa0ac338a6a068a519676b82e
Alan <wangyueleng@gmail.com>
2023-08-30 21:51:35 -0400

    lc/q2483-minimum-penalty-for-a-shop

---
+++ lc/q2483-minimum-penalty-for-a-shop/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        best_index = 0
+        current_penalty = 0
+        best_penalty = 0
+        n = len(customers)
+        for i in range(n):
+            if customers[i] == "Y":
+                current_penalty -= 1
+            elif customers[i] == "N":
+                current_penalty += 1
+
+            if current_penalty < best_penalty:
+                best_index = i + 1
+                best_penalty = current_penalty
+        return best_index
