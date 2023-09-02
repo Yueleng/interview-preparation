2d7780555b243b0a83604af9fd687775c6abc944
Alan <wangyueleng@gmail.com>
2023-09-01 21:29:56 -0400

    lc/q338-counting-bits

---
+++ lc/q338-counting-bits/solution.py
@@ -0,0 +1,9 @@
+from ast import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        counter = [0]
+        for i in range(1, n + 1):
+            counter.append(counter[i >> 1] + i % 2)
+        return counter
