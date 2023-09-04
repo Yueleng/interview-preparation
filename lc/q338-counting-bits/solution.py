4f054fdde98642a86b74fae5c491c8c70275e6ac
Alan <wangyueleng@gmail.com>
2023-09-04 16:49:08 -0400

    lc/q338-counting-bits/explanation

--- lc/q338-counting-bits/solution.py
+++ lc/q338-counting-bits/solution.py
@@ -7,3 +7,9 @@ class Solution:
         for i in range(1, n + 1):
             counter.append(counter[i >> 1] + i % 2)
         return counter
+
+
+# key point:
+#  how to get counter[i] in O(1) time by rely on previous results:
+#  counter[0] to counter[i-1]
+# bit operation: ``>>``: Bitwise Right Shift Operator
