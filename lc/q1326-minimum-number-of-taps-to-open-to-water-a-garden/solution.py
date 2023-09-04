3b7e85da2800636e4e2efe2483315af4e919b6ae
Alan <wangyueleng@gmail.com>
2023-09-04 17:55:09 -0400

    lc/q1326-minimum-number-of-taps-to-open-to-water-a-garden/explanation

--- lc/q1326-minimum-number-of-taps-to-open-to-water-a-garden/solution.py
+++ lc/q1326-minimum-number-of-taps-to-open-to-water-a-garden/solution.py
@@ -3,10 +3,17 @@ from ast import List
 
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
+        # create a list to track the maximum reach for each position
         max_reach = [0] * (n + 1)
+
+        # calculate the maximum reach for each tap
         for i in range(n + 1):
+            # calculate the leftmost position the tap can reach
             left_i = max(0, i - ranges[i])
+            # calculate the rightmost position the tap can reach
             right_i = min(n, i + ranges[i])
+            # update the maximum reach(value: right position) for the leftmost position
+            # i.e. max[1] = 10,  means there is an interval [1, 10]
             max_reach[left_i] = max(max_reach[left_i], right_i)
 
         current_right = 0
@@ -20,5 +27,7 @@ class Solution:
                 current_right = next_right
 
             # for every i, try to update next_right
+            # important: this make sure that we have the furthest next_right
+            # when chosing next_right
             next_right = max(next_right, max_reach[i])
         return taps
