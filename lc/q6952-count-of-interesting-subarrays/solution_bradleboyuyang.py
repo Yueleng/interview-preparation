e20b18d5268655309aa0be7cf8ca83c0b484f3de
Alan <wangyueleng@gmail.com>
2023-09-04 14:18:44 -0400

    lc/q6952-count-of-interesting-subarrays/explanation

--- lc/q6952-count-of-interesting-subarrays/solution_bradleboyuyang.py
+++ lc/q6952-count-of-interesting-subarrays/solution_bradleboyuyang.py
@@ -5,13 +5,29 @@ from collections import defaultdict
 class Solution:
     def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
         n = len(nums)
-        prefix = [0] * (n + 1)
+        # keep records of all indices that up to ith index,
+        # count of nums[j] % modulo == k for j from 0 to i(includisve)
+        prefix = [0 for _ in range(n)]
         for i in range(n):
-            prefix[i + 1] = prefix[i] + (nums[i] % modulo == k)
+            prefix[i] = (prefix[i - 1] if i > 0 else 0) + (nums[i] % modulo == k)
         count = defaultdict(int)
         ans = 0
-        for i in range(n + 1):
+        count[0] = 1
+        for i in range(n):
             j = prefix[i] - k
-            ans += count[j % modulo]
+            remain = j % modulo
+            ans += count[remain]
             count[prefix[i] % modulo] += 1
         return ans
+
+
+# Subarray sums divisible by modulo and remains k
+# prefix = [a_0, a_1, ..., a_n]
+# we want all subarrays prefix[i:j] s.t.
+# sum(a[i:j]) % modulo == k
+# so if we have F[i], then we need to find an F[j]
+# where j < i, s.t.
+#   1. (F[i] - F[j]) % modulo = k
+#   2. (F[i] - k) % modulo = F[j] % modulo
+# F[i] calculate as you go through the array
+# F[j] stored in hashmap
