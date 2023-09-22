a6e8349979b034e9c5278313d778cc231558bb65
Alan <wangyueleng@gmail.com>
2023-09-21 23:55:33 -0400

    lc/q1658-minimum-operation-to-reduce-x-to-zero

--- lc/q1658-minimum-operation-to-reduce-x-to-zero/solution.py
+++ lc/q1658-minimum-operation-to-reduce-x-to-zero/solution.py
@@ -8,10 +8,20 @@ class Solution:
         target = sum(nums) - x
         acc_sum = 0
         left_idx = 0
-        for i in range(len(nums)):
+        max_count = 0
+        flag = 0
+        n = len(nums)
+        for i in range(n):
             acc_sum += nums[i]
 
             # window too wide, shink it
-            if acc_sum > target:
+            while acc_sum > target:
                 acc_sum -= nums[left_idx]
                 left_idx += 1
+
+            # window just fit
+            if acc_sum == target:
+                flag = 1
+                max_count = max(max_count, i - left_idx + 1)
+
+        return n - max_count if flag else -1
