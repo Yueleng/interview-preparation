918d81e0d9f6896e9e08ae2a3c5778bf4ba46b7a
Alan <wangyueleng@gmail.com>
2023-10-31 21:08:24 -0400

    lc/q2433-find-the-original-array-of-prefix-xor

---
+++ lc/q2433-find-the-original-array-of-prefix-xor/solution.py
@@ -0,0 +1,12 @@
+from ast import List
+
+
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        prev = pref[0]
+
+        for i in range(1, len(pref)):
+            pref[i] ^= prev
+            prev ^= pref[i]
+
+        return pref
