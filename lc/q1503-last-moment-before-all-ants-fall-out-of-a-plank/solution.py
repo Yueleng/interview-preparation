442aef94d3f75d5999b845e028d01a504b2ada9c
Alan <wangyueleng@gmail.com>
2023-11-03 23:37:56 -0400

    lc/q1503-last-moment-before-all-ants-fall-out-of-a-plank

---
+++ lc/q1503-last-moment-before-all-ants-fall-out-of-a-plank/solution.py
@@ -0,0 +1,8 @@
+from ast import List
+
+
+class Solution:
+    def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
+        return max(
+            max(left) if len(left) > 0 else 0, n - min(right) if len(right) > 0 else 0
+        )
