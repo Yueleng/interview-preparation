e3e161fa81b821e7b683f9076d31d38559474129
Alan <wangyueleng@gmail.com>
2023-11-16 01:33:53 -0500

    lc/q1980-find-unique-binary-string

---
+++ lc/q1980-find-unique-binary-string/solution.py
@@ -0,0 +1,6 @@
+from ast import List
+
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        return "".join(["1" if num[i] == "0" else "0" for i, num in enumerate(nums)])
