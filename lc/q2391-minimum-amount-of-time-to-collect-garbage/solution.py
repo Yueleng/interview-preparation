aaeec507bc556827b76ad6282e47cbc77bc14cc0
Alan <wangyueleng@gmail.com>
2023-11-21 23:24:53 -0500

    lc/q2391-minimum-amount-of-time-to-collect-garbage

---
+++ lc/q2391-minimum-amount-of-time-to-collect-garbage/solution.py
@@ -0,0 +1,21 @@
+from ast import List
+
+
+class Solution:
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        res = 0
+
+        # collect tocal length
+        for g in garbage:
+            res += len(g)
+
+        m, p, g = False, False, False
+
+        for i in range(len(travel), 0, -1):
+            m = m or "M" in garbage[i]
+            p = p or "P" in garbage[i]
+            g = g or "G" in garbage[i]
+
+            res += travel[i - 1] * (m + p + g)
+
+        return res
