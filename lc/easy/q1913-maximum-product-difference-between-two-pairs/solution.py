d063f6d981a284dddfc634cbd3274190afa336f2
Alan <wangyueleng@gmail.com>
2023-12-24 14:23:29 -0500

    lc/easy/q1913-maximum-product-difference-between-two-pairs

---
+++ lc/easy/q1913-maximum-product-difference-between-two-pairs/solution.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        return nums[0] * nums[1] - nums[-2] * nums[-1]
