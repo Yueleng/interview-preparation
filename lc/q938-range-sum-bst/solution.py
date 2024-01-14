7297983eb5f54112e5ba4b09d856c4d42056e7d9
Alan <wangyueleng@gmail.com>
2024-01-14 17:38:46 -0500

    lc/q938-range-sum-bst (#3)

---
+++ lc/q938-range-sum-bst/solution.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        def dfs(node: Optional[TreeNode]):
+            if node is None:
+                return 0
+            if node.val < low:
+                return dfs(node.right)
+            if node.val > high:
+                return dfs(node.left)
+            return node.val + dfs(node.left) + dfs(node.right)
+
+        return dfs(root)
